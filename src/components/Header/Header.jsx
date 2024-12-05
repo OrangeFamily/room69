65066a564b70e5e561946c4369e9c46c54952ff5
Олексій Демидов <agdemidof@gmail.com>
2024-12-05 20:09:12 +0200

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,13 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-// import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import s from './Header.module.scss'
+import s from './Header.module.scss';
 
 const Header = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -20,16 +19,23 @@ const Header = () => {
   };
 
   const drawerContent = (
-    <Box sx={{ width: 250 }}>
+    <Box 
+      sx={{ 
+        width: 250, 
+        backgroundColor: '#333', // Зміна кольору фону
+        height: '100%', // Зайняти всю висоту
+        color: '#fff', // Білий текст для контрасту
+      }}
+    >
       <List>
         <ListItem button component="a" href="https://misteram.com.ua/chernigov/orangebar" target="_blank" rel="noreferrer">
-          <ListItemText primary="ДОСТАВКА" />
+          <ListItemText primary="ДОСТАВКА" sx={{ color: '#fff' }} />
         </ListItem>
         <ListItem button component="a" href="tel:+380939179177">
-          <ListItemText primary="+38 093 91 79 177" />
+          <ListItemText primary="+38 093 91 79 177" sx={{ color: '#fff' }} />
         </ListItem>
         <ListItem button component="a" href="https://www.instagram.com/room69.restaurant?igsh=NHp4Mnk4bWdycWp0" target="_blank" rel="noreferrer">
-          <ListItemText primary="Instagram" />
+          <ListItemText primary="Instagram" sx={{ color: '#fff' }} />
         </ListItem>
       </List>
     </Box>
@@ -37,15 +43,12 @@ const Header = () => {
 
   return (
     <>
-      <AppBar position="static">
+      <AppBar 
+        position="fixed"
+        sx={{ backgroundColor: '#333', boxShadow: 'none' }}
+      >
         <Toolbar>
-          {/* Логотип */}
-          {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Restaurant Name
-          </Typography> */}
-
-          {/* Навігація для великих екранів */}
-          <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'block' } }}>
             <Button
               color="inherit"
               href="https://misteram.com.ua/chernigov/orangebar"
@@ -57,11 +60,14 @@ const Header = () => {
             <Button color="inherit" href="tel:+380939179177">
               +38 093 91 79 177
             </Button>
-            <Button color="inherit" href="https://www.instagram.com/room69.restaurant?igsh=NHp4Mnk4bWdycWp0" 
-              target="_blank" 
-              rel="noreferrer" 
-              className={s.link}>
-                <img src={require('./img/inst.png')} alt="" className={s.icon} />
+            <Button
+              color="inherit"
+              href="https://www.instagram.com/room69.restaurant?igsh=NHp4Mnk4bWdycWp0"
+              target="_blank"
+              rel="noreferrer"
+              className={s.link}
+            >
+              <img src={require('./img/inst.png')} alt="" className={s.icon} />
             </Button>
           </Box>
 
@@ -81,7 +87,17 @@ const Header = () => {
       </AppBar>
 
       {/* Drawer для мобільного меню */}
-      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+        PaperProps={{
+          sx: {
+            backgroundColor: '#333', // Зміна кольору Drawer
+            color: '#fff', // Білий текст
+          },
+        }}
+      >
         {drawerContent}
       </Drawer>
     </>
